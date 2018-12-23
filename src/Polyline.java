adc72d081c887737793471994901402e8e3839ce
liranglozman <liranglozman123@gmail.com>
2018-12-23 21:33:17 +0200

    Added new Polyline.java, self created polyline that should draw a polygon line
    Signed-off-by: liranglozman <liranglozman123@gmail.com>

---
+++ src/Polyline.java
@@ -0,0 +1,18 @@
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+
+public class Polyline {
+
+	
+	public void addPoint()
+	{
+		
+	}
+	
+	
+}
